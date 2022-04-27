8934ec91f0a93c551101a907223dde29f5bd46fd
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-04-27 15:20:32 +0900

    Vue.js Lesson 02 - Create listeners (click and key) - COMPLETED
    Completed the second Vue lesson from the UDEMY course. I created three
    event listeners and returned value that were displayed in the app.
    The events listened are mouse click, any keyboard key and enter key.

---
+++ lesson_02/app.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            message: "I'm learning and I'll change careers into tech!",
+            output: "",
+            output2: ""
+        }
+    },
+    methods: {
+        alertMessage() {
+            alert(this.message);
+
+        },
+        showOutput(e) {
+            this.output = e.target.value
+        },
+        showOutput2(e) {
+            this.output2 = e.target.value
+        }
+    }
+});
+
+app.mount("#assignment");
