3d54f4f67beaa081357c09ac1db10df5d0d266b3
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-04-29 23:15:11 +0900

    Implement the buttons reactivity and output message.
    Started the Vue.js lesson 03. Implemented the buttons and the correct
    message output. However, I created a data to display the message and
    a method to change the data variable but it didn't work. When I changed
    the html to display the returned method, it worked. It would be good
    to know why.

---
+++ lesson_03/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            valueCalc: 0,
+        }
+    },
+    methods: {
+        increaseFive() {
+            this.valueCalc = this.valueCalc + 5;
+        },
+        increaseOne() {
+            this.valueCalc++;
+        },
+        displayOutput() {
+            if (this.valueCalc < 37) {
+                return "Not there yet.";
+            } else if (this.valueCalc > 37) {
+                return "Too much!";
+            } else {
+                return valueCalc;
+            }
+    
+        }
+    }
+});
+
+app.mount("#assignment");
