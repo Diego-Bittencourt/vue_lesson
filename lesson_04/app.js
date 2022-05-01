7d14ef8386309a35559bbe3a266688e3327d1e3a
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-05-02 00:07:06 +0900

    Implement the toggle visible/invisible button.
    I created two data properties to hidden and visible classes, each with
    boolean expressions. Then, created a method to be invoked upon clicking
    and a computed property to return an object with the classes.

---
+++ lesson_04/app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            hideParagraph: false,
+            showParagraph: true,
+            
+        }
+    },
+    computed: {
+        paragraphClasses() {
+            return {
+                hidden: this.hideParagraph,
+                visible: this.showParagraph
+            };
+        }
+    },
+    methods: {
+        toggleVisible() {
+            if(this.hideParagraph != this.showParagraph) {
+                this.hideParagraph = !this.hideParagraph;
+                this.showParagraph = !this.showParagraph;
+            } else {
+                alert("Error in the toggle paragraph!");
+            }
+
+        }
+    }
+
+});
+
+app.mount("#assignment")
