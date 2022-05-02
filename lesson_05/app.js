6f4297b54e533d368070033a717402f36dc42d9a
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-05-03 00:02:15 +0900

    Implement the visibility toggle button.
    I implemented the button to toggle the visibility of the list by
    adding a listener to the click, which activates a method that changes
    a data property with a boolean expression. The list holds a v-if
    statement that checks the data property with the boolean expression.
    I added a <p> element with a v-else statement as a placeholder to keep
    the two buttons apart and the list looks more organized.

--- lesson_05/app.js
+++ lesson_05/app.js
@@ -2,14 +2,20 @@ const app = Vue.createApp({
     data() {
         return {
             toDos: [],
-            userInput: ""
+            userInput: "",
+            listVisible: true
         }
     },
     methods: {
         addToDo() {
-            this.toDos.push(this.userInput);
-            this.userInput = "";
-        }
+            if (this.userInput !== "") {
+                this.toDos.push(this.userInput);
+                this.userInput = "";
+            }
+        },
+        toggleVisibility() {
+            this.listVisible = !this.listVisible;
+        }  
     }
 });
 
@@ -19,3 +25,8 @@ app.mount("#assignment");
 // 01 - Created the Vue.app;
 // 02 - Created the data properties to hold the goal arrays and the user's input.
 // 03 - Create the button click and enter event listener and assigned them to the addToDo method.
+// 04 - Created an if statement to manage invalid input from the user (empty string or one whitespace).
+        // It might a good idea to use regex to manage that.
+// 05 - Created a method to toggle visibility. The method is activated by the click in the button and 
+        // change the listVisibile data property.
+// 06 - I created a new <p> element using v-else as a placeholder to keep the list organized.
