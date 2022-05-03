11fd4c1f367a539a7ffeace50a95bfee7bf30b0e
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-05-04 00:12:20 +0900

    Implement the Special button. Available every 3 rounds.
    I just added the special button functionality. It deals more damage,
    but it is only used every three rounds.

--- monster_battle/app.js
+++ monster_battle/app.js
@@ -6,7 +6,8 @@ const app = Vue.createApp({
     data() {
         return {
             monsterHealth: 100,
-            playerHealth: 100
+            playerHealth: 100,
+            playRound: 0
         }
     },
     computed: {
@@ -15,18 +16,28 @@ const app = Vue.createApp({
         },
         playerHealthBar() {
             return { width: this.playerHealth + "%" }
+        },
+        specialAvailable() {
+            return this.playRound % 3 !== 0;
         }
     },
     methods: {
         attackMonster() {
-            let attackValue = getRandomValue(12, 5);
+            let attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
             this.attackPlayer();
+            this.playRound++;
             },
         attackPlayer() {
-            let attackValue = getRandomValue(15, 8);
+            let attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
-        }    
+            },
+        specialAttack() {
+            let attackValue = getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+            this.playRound++;
+        }
     }
 
 });
@@ -40,3 +51,6 @@ app.mount("#game");
 //      as long as the function isn't send to the html.
 // 03 - Create two methods to perform the attack functionality. The attackMonster (performed by the player) refers to 
 //      the attackPlayer (performed by the monster) inside the method.
+// 04 - Create the click listener the computed data that changes the inline style element to reduce the healthbars.
+//      In this scenario, creating a inline style command is ideal because ir too troublesome to create class to every
+//      health bar lenght possible.
