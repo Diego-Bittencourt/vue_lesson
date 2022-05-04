4d70515427c15b69cfb32d49a9cdb8e19476b30b
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-05-05 00:06:18 +0900

    Implement the game over screen.
    I implemented the game over screen. The health are being watched by
    watchers. And the whole game over screen is being triggered by a
    v-if that checks for the winner data property. Initially, the winner
    property is set to null, which is a falsy value. When one of the
    watchers is triggered, the winner property is changed and it triggers
    the v-ifs on the h3 html elements.

--- monster_battle/app.js
+++ monster_battle/app.js
@@ -7,7 +7,8 @@ const app = Vue.createApp({
         return {
             monsterHealth: 100,
             playerHealth: 100,
-            playRound: 0
+            playRound: 0,
+            winner: null
         }
     },
     computed: {
@@ -22,6 +23,22 @@ const app = Vue.createApp({
         },
         healAvailable() {
             return this.playRound % 2 != 0;
+        }
+    },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                this.winner = "draw";
+            } else if (value <= 0) {
+                this.winner = "monster";
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                this.winner = "draw";
+            } else if (value <= 0) {
+                this.winner = "player";
+            }
         }
     },
     methods: {
@@ -67,3 +84,11 @@ app.mount("#game");
 // 04 - Create the click listener the computed data that changes the inline style element to reduce the healthbars.
 //      In this scenario, creating a inline style command is ideal because ir too troublesome to create class to every
 //      health bar lenght possible.
+// 05 - Create the special attack button. The special attack button has a bigger damage number, but can only be used every three 
+//      rounds. To keep track of rounds, I created a data property and every attack button adds to the round data property.
+// 06 - Create the heal button. The heal has an if statement to be sure the total HP and the heal amount don't exceed 100.
+//      Used an if statement for that. After, the heal method triggers the monster attack and the round count. I created a
+//      track to only use the heal ability every other round. I use the round tracker and the % operator.
+// 07 - Time to implement a game over window. Create two watcher to watch the player health and the monster health. 
+// 08 - Create a data property to hold the winner. The winner property is set to null initially because I'll use a
+//      v-if to triger the game over screen, therefore using null is a falsy value which doesn't trigger the v-if.
