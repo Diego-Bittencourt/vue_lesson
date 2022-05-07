bfa2c75053eb9facf91aabc831bae12f4226d4c8
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-05-07 23:24:49 +0900

    Create the first Vue app using the server with npm and Node.js
    The files from the src folder using the npm and Node.js

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '0123 45678 90',
+                    email: 'manuel@localhost.com'
+                },
+                {
+                    id: 'julia',
+                    name: 'Julia Jones',
+                    phone: '987 12345 90',
+                    email: 'julie@localhost.com' 
+                }
+            ]
+        }
+    }
+
+}
+</script>
---
+++ vue-cli-01-a-new-vue-project/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'; 
+
+// refering to a package, so just the package name.
+
+import App from './App.vue'; 
+
+//refering to a file, so the path to the file. The name after import is up to you. It's conventional
+// to name the same as the file.
+
+createApp({}).mount('');
