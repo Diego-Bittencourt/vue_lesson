fa8b336b718b48ef7d00e02623971172e293a5ed
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-05-07 23:51:24 +0900

    Working on the project.

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+   <li>
+       <h2>{{ name }}</h2>
+       <button>Show Details</button>
+       <ul>
+           <li><strong>Phone:</strong> {{ friend.phone }} </li>
+           <li><strong>Email: </strong> {{ friend.email }} </li>
+           <li><strong>
+        </ul>
+    </li> 
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            toggleDetails: false,
+            friend: {
+                id: "manuel",
+                name: "Manuel Lorenz",
+                email: "manuel@localhost.com",
+                phone: '0123 45678 90',
+            }
+        }
+    },
+    methods: {
+        detailsVisible() {
+            this.toggleDetails = !this.toggleDetails;
+        }
+    }
+};
+</script>
