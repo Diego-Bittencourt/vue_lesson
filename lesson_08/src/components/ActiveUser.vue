75250de645ccc7b829d19aa30135e93945d2c992
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-05-11 08:15:18 +0900

    Start of the assignment. Comps communication.
    I started the assignmentfor the chapter 08 regarding components
    communication. I created the two components and started the
    <template> tag.

---
+++ lesson_08/src/components/ActiveUser.vue
@@ -0,0 +1,20 @@
+<template>
+    <active-user>
+        <div class="comp1wrap">
+            <h2>Nome: {{ userName }} </h2>
+            <h3> Age: {{userAge}} </h3>
+        </div>
+    </active-user>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            userName: "",
+            userAge: ""
+    }
+    }  
+};
+</script>
---
+++ lesson_08/src/components/UserData.vue
@@ -0,0 +1,17 @@
+<template>
+    <user-input>
+    </user-input>    
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default {
+    data() {
+        return {
+            userName: "",
+            userAge: ""
+}
+    }
+};
+</script>
