dc78cdf26a9c035ffdfce963490950f03c7bb8ab
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-05-13 14:33:40 +0900

    Explaining the issue regarding the passing through component.
    Initially, the App.vue file is sending the props to the KnowleddgeBase
    file, but this last one doesn't use the props for anything.
    The KnowledgeBase file just fowards the props (topics) to the
    KnowledgeGrid file which uses it to perform the v-for code.
    Plus, the KnowledgeElement send an emit event that is being passed
    through knowledgegrid and knowledgebase and is being used in the
    App.vue file. This passthrough system isn't uncommon in Vue but
    there are better ways to do this, by useng provide and inject.

--- cmp-communication/src/main.js
+++ cmp-communication/src/main.js
@@ -14,3 +14,10 @@ app.component('knowledge-element', KnowledgeElement);
 app.component('knowledge-grid', KnowledgeGrid);
 
 app.mount('#app');
+
+// Initially, the App.vue file is sending the props to the KnowleddgeBase file, but this last one doesn't use the
+//props for anything. The KnowledgeBase file just fowards the props (topics) to the KnowledgeGrid file which
+// uses it to perform the v-for code.
+//Plus, the KnowledgeElement send an emit event that is being passed through knowledgegrid and knowledgebase and
+//is being used in the App.vue file. This passthrough system isn't uncommon in Vue but there are better ways
+//to do this, by useng provide and inject.
