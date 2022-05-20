92ab8b6d0395e335046fa36b2f36ea5194423d15
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2022-05-20 22:52:53 +0900

    Added the fetch method with the JSON object to send data to server.
    I created a fetch method that sends an http request when the form
    is submited. I'm using the JSON object to stringify the argument and
    send it to the server.

--- chapter_12/src/components/survey/LearningSurvey.vue
+++ chapter_12/src/components/survey/LearningSurvey.vue
@@ -13,22 +13,14 @@
           <label for="rating-poor">Poor</label>
         </div>
         <div class="form-control">
-          <input
-            type="radio"
-            id="rating-average"
-            value="average"
-            name="rating"
-            v-model="chosenRating"
-          />
+          <input type="radio" id="rating-average" value="average" name="rating" v-model="chosenRating" />
           <label for="rating-average">Average</label>
         </div>
         <div class="form-control">
           <input type="radio" id="rating-great" value="great" name="rating" v-model="chosenRating" />
           <label for="rating-great">Great</label>
         </div>
-        <p
-          v-if="invalidInput"
-        >One or more input fields are invalid. Please check your provided data.</p>
+        <p v-if="invalidInput">One or more input fields are invalid. Please check your provided data.</p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -46,7 +38,7 @@ export default {
       invalidInput: false,
     };
   },
-  emits: ['survey-submit'],
+  // emits: ['survey-submit'],
   methods: {
     submitSurvey() {
       if (this.enteredName === '' || !this.chosenRating) {
@@ -55,10 +47,24 @@ export default {
       }
       this.invalidInput = false;
 
-      this.$emit('survey-submit', {
-        userName: this.enteredName,
-        rating: this.chosenRating,
+      // this.$emit('survey-submit', {
+      //   userName: this.enteredName,
+      //   rating: this.chosenRating,
+      // });
+
+      fetch('https://vue-http-demo-69dfb-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json', {
+        method: 'POST', //it tells the type of the http request. POST to send data, GET, DELETE and PATCH (patch is delete and update)
+        header: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: this.enteredName,
+          rating: this.chosenRating
+        }),
       });
+      //this fetch function is a built-in function in JS to send http requests.
+      //the link is from the firebase dashoard added surveys.json;
+      //the extension .json is firebase specific.
 
       this.enteredName = '';
       this.chosenRating = null;
