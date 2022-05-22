54979045e8ec92aab163e9e6c41d2835d2e4b3b0
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2022-05-22 14:43:38 +0900

    Create the fetch/GET functionality.
    I created a GET http request. It returned an object and I loop through
    every objects and get the name and rating of each object.

--- chapter_12/src/components/survey/UserExperiences.vue
+++ chapter_12/src/components/survey/UserExperiences.vue
@@ -3,15 +3,11 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <ul>
-        <survey-result
-          v-for="result in results"
-          :key="result.id"
-          :name="result.name"
-          :rating="result.rating"
-        ></survey-result>
+        <survey-result v-for="result in results" :key="result.id" :name="result.name" :rating="result.rating">
+        </survey-result>
       </ul>
     </base-card>
   </section>
@@ -19,13 +15,46 @@
 
 <script>
 import SurveyResult from './SurveyResult.vue';
+import BaseButton from '../UI/BaseButton.vue';
 
 export default {
   // props: ['results'],
   components: {
     SurveyResult,
+    BaseButton
+},
+  data() {
+    return {
+      results: []
+    }
   },
+  methods: {
+    loadExperiences() {
+      fetch('https://vue-http-demo-69dfb-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json').then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      }).then((data) => {
+        const results = [];
+        for (const id in data) {
+          results.push({
+            id: id,
+            name: data[id].name,
+            rating: data[id].rating,
+          })
+        }
+        this.results = results;
+      });
+      
+    }
+  }
 };
+      //Now I'm sending a GET request. Since the GET request is the defaul, there is no need to write it.
+      //Every GET request returns a promise that can be used with the .then() method which takes a function inside.
+      //The object returned from that has a method .ok() to check if the procedure was succesful.
+      //The data comes in a json format, so we can use the json method to return it.
+      //The json method also returns a promise. We can see the data inside the json file if we use another .then with a console.log
+      //The arrow function syntax is used because the this. keyword inside an arrow function has the same functionality as it was outsite the arrow function.
 </script>
 
 <style scoped>
