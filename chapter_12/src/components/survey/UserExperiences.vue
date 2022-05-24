6555c7652ca829977fb35caf1ee7f6ec3790eb67
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2022-05-24 21:00:14 +0900

    Handle errors
    I created another element and stacked v-if to create different
    text for different situations like loading, no data and errors.

--- chapter_12/src/components/survey/UserExperiences.vue
+++ chapter_12/src/components/survey/UserExperiences.vue
@@ -6,8 +6,9 @@
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error"> {{ error }} </p>
       <p v-else-if="!isLoading && (!results || results.length === 0)">No data. Please, add some data.</p>
-      <ul v-else>
+      <ul v-else-if="!isLoading && results && results.length > 0">
         <survey-result v-for="result in results" :key="result.id" :name="result.name" :rating="result.rating">
         </survey-result>
       </ul>
@@ -24,15 +25,17 @@ export default {
   components: {
     SurveyResult,
     BaseButton
-},
+  },
   data() {
     return {
       results: [],
-      isLoading: false
+      isLoading: false,
+      error: null
     }
   },
   methods: {
     loadExperiences() {
+      this.error = null;
       this.isLoading = true;
       fetch('https://vue-http-demo-69dfb-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json').then((response) => {
         if (response.ok) {
@@ -49,15 +52,20 @@ export default {
           })
         }
         this.results = results;
-      });
-      
+      })
+        .catch((error) => {
+          console.log(error);
+          this.isLoading = false;
+          this.error = "Failed to load data. Please, try again later.";
+        });
+
     },
-    
+
   },
   mounted() {
-      this.loadExperiences();
-      //The mounted method triggers when the component is mounted. Go to the Vue component lifecycle for a refresher.
-    }
+    this.loadExperiences();
+    //The mounted method triggers when the component is mounted. Go to the Vue component lifecycle for a refresher.
+  }
 };
       //Now I'm sending a GET request. Since the GET request is the defaul, there is no need to write it.
       //Every GET request returns a promise that can be used with the .then() method which takes a function inside.
