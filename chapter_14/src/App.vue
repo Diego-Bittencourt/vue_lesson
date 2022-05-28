bf3b51cf20c2d02adc127dc91e9c3693f8328742
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-05-29 01:29:15 +0900

    Add leaving animation in the dialog box.

--- chapter_14/src/App.vue
+++ chapter_14/src/App.vue
@@ -5,15 +5,25 @@
   </div>
   <div class="container">
     <!-- <transition> can only have 1 direct child, with exceptions.  -->
-    <transition>
+    <!-- You can create personalize classes added by Vue by adding the name property.
+      This way, only the first word is replaced in the class. So, for ex
+      if you have the v-enter-to becomes para-enter-to -->
+    <!-- You can also change the class name entirely by stating it in the transition elemenet.
+      You can write v-enter-from="first-state"
+      This is particularly interesting when working with css libraries and stuff. -->
+    <transition name="para">
       <p v-if="paraIsVisible">This is only sometimes visible...</p>
     </transition>
     <button @click="toggleParagraph">Toggle paragraph</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
-    <p>This is a test dialog!</p>
-    <button @click="hideDialog">Close it!</button>
-  </base-modal>
+  <transition name="modal">
+    <!-- When using transition in custom elements, the classes fall throuth the root of the child element
+    Since, in this case, there are two root elements, so it doesn't work. -->
+    <base-modal @close="hideDialog" v-if="dialogIsVisible">
+      <p>This is a test dialog!</p>
+      <button @click="hideDialog">Close it!</button>
+    </base-modal>
+  </transition>
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
@@ -99,33 +109,78 @@ in the styling.
 They are named accordingly to enter or leave 
 the classes are being removed, so there is no use for the forward keyword.*/
 
-.v-enter-from {
+.para-enter-from {
   animation: slide-scale 0.3s ease-out;
   /* opacity: 0;
   transform: translateY(-30px); */
 }
 
-.v-enter-active {
+.para-enter-active {
   animation: slide-scale 0.3s ease-out;
 }
 
-.v-enter-to {
+.para-enter-to {
   /* opacity: 1;
   transform: translateY(0); */
 }
 
-.v-leave-from {
-/* opacity: 1;
+.para-leave-from {
+  /* opacity: 1;
   transform: translateY(0); */
 }
 
-.v-leave-active {
-animation: slide-scale 0.3s ease-out;
+.para-leave-active {
+  animation: slide-scale 0.3s ease-out;
 }
 
-.v-leave-to {
+.para-leave-to {
   /* opacity: 0;
   transform: translateY(30px); */
+}
+
+.modal-enter-from {
+}
+
+.modal-enter-active {
+  animation: modal 0.3s ease-out;
+}
+
+.modal-enter-to {
+}
+
+.modal-leave-from {
+
+}
+
+.modal-leave-active {
+animation: modal 0.3s ease-in;
+}
+
+.modal-leave-to {
+
+}
+
+@keyframes modal-leave {
+  from {
+    opacity: 1;
+    tranform: translateY(0) scale(1);
+  }
+
+  to {
+
+  }
+}
+
+@keyframes modal {
+  from {
+    opacity: 0;
+    transform: translateY(-50px) scale(0.9);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0) scale(1);
+  }
 }
 
 @keyframes slide-scale {
