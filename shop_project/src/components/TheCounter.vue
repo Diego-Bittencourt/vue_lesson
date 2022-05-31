72bd910d07233f322b6f4398d155487752c34460
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-05-31 22:44:36 +0900

    Initial commit regarding the Vuex learning.
    Vuex creates a store in the main.js file that holds global data.
    It creates a central plate where all the app's data are in one place
    and it's accessible through the app.

---
+++ shop_project/src/components/TheCounter.vue
@@ -0,0 +1,18 @@
+<template>
+  <h3>{{ this.$store.state.counter }}</h3>
+<button @click="addagain">Add 1</button>
+</template>
+
+<script>
+export default {
+    methods: {
+        addagain() {
+            return this.$store.state.counter++;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
