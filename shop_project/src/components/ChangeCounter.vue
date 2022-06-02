37e422e9f077075053102712716fb4100d17fc06
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-06-02 13:14:03 +0900

    Fix bugs and re-add mapGetters and mapActions.
    I restored a stable version of my code and fixed some bugs and went
    again through the proccess of adding ...mapGetters and ...mapActions.

--- shop_project/src/components/ChangeCounter.vue
+++ shop_project/src/components/ChangeCounter.vue
@@ -1,14 +1,24 @@
 <template>
-  <button @click="addOne">Add 2</button>
+  <button @click="inc">Add 2</button>
+  <button @click="increase({value: 11})">Add 11</button>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
     methods: {
-        addOne() {
-            this.$store.dispatch('increment');
-            //to use a mutation inside the components, you should use the syntax above.
-        }
+        // addOne() {
+        //     this.$store.dispatch('increment');
+        //     //to use a mutation inside the components, you should use the syntax above.
+        // }
+        //The mapActions work similarly to mapGetters.
+        // actions inside methods. getter inside computed.
+        // ...mapActions(['increment', 'increase'])
+        ...mapActions({
+            inc: 'increment',
+            increase: 'increase'
+
+        })
     }
 }
 </script>
--- shop_project/src/components/TheCounter.vue
+++ shop_project/src/components/TheCounter.vue
@@ -1,15 +1,22 @@
 <template>
-  <h3>{{ counter }}</h3>
+  <h3>{{ finalCounter }}</h3>
 <button @click="counter">Add 1</button>
 </template>
 
 <script>
+import { mapGetters } from'vuex';
 export default {
     computed: {
-        counter() {
-            return this.$store.getters.finalCounter;
-        }
-    }
+        // counter() {
+        //     return this.$store.getters.finalCounter;
+        // }
+        //mapGetters takes an array or object as argument and returns the properties listed in that arguments.
+        //...mapGetters({
+        //     finalCounter: 'finalCounter'
+        // })
+        ...mapGetters(['finalCounter'])
+    },
+
 }
 </script>
 
